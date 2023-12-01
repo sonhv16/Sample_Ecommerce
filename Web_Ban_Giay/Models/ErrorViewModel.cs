ce99198b207e26ce809a317631044f9080925409
sonhv <sonhv16@fpt.edu.vn>
2023-12-02 02:03:27 +0700

    re-arrange folders

--- Web_Ban_Giay/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Web_Ban_Giay.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
